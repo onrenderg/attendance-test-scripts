cc59ccc17d342e820c937cf3ca5fa543b91c1d40
Your Name <you@example.com>
2026-01-27 12:56:09 +0530

    done

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,28 @@
-﻿namespace VectorP
+﻿#if ANDROID
+using Android.Webkit;
+#endif
+
+namespace VectorP
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            Microsoft.Maui.Handlers.WebViewHandler.Mapper.AppendToMapping("MyCustomization", (handler, view) =>
+            {
+#if ANDROID
+                handler.PlatformView.Settings.JavaScriptEnabled = true;
+                handler.PlatformView.Settings.AllowFileAccess = true;
+                handler.PlatformView.Settings.AllowFileAccessFromFileURLs = true;
+                handler.PlatformView.Settings.AllowUniversalAccessFromFileURLs = true;
+                handler.PlatformView.Settings.MixedContentMode = MixedContentHandling.AlwaysAllow;
+                handler.PlatformView.Settings.DomStorageEnabled = true;
+                handler.PlatformView.Settings.DatabaseEnabled = true;
+#endif
+            });
+
             LoadWebView();
         }
 
@@ -12,7 +30,7 @@
         {
 #if ANDROID
             // Resources/Raw files are mapped to android_asset in MAUI
-            webview_loaddata.Source = "file:///android_asset/wwwroot/index.html";
+            webview_loaddata.Source = "file:///android_asset/index.html";
 #else
             webview_loaddata.Source = "index.html";
 #endif
