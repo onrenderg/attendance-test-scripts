dff04eea5c63a82ba3bef079db49ac4de224d5ca
Your Name <you@example.com>
2026-01-27 12:09:01 +0530

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace VectorP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+
+        private async void InitializeWebView()
+        {
+#if ANDROID
+            // On Android, copy HTML to a location accessible by WebView and load from file URL
+            var fileName = "index.html";
+            var targetPath = Path.Combine(FileSystem.CacheDirectory, fileName);
+            
+            // Copy HTML from app package to cache directory
+            using (var stream = await FileSystem.OpenAppPackageFileAsync(fileName))
+            using (var fileStream = File.Create(targetPath))
+            {
+                await stream.CopyToAsync(fileStream);
+            }
+            
+            // Load from file:// URL which allows proper fetch() origin
+            webview_loaddata.Source = new UrlWebViewSource { Url = $"file://{targetPath}" };
+#else
+            // For other platforms, load inline HTML
+            string htmlContent = await LoadHtmlFromRawAsync();
+            webview_loaddata.Source = new HtmlWebViewSource { Html = htmlContent };
+#endif
+        }
+
+        private async Task<string> LoadHtmlFromRawAsync()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("index.html");
+            using var reader = new StreamReader(stream);
+            return await reader.ReadToEndAsync();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace VectorP
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .ConfigureMauiHandlers(handlers =>
+                {
+#if ANDROID
+                    handlers.AddHandler<WebView, VectorP.Platforms.Android.CustomWebViewHandler>();
+#endif
+                });
+
+#if DEBUG
+            builder.Services.AddLogging(configure =>
+            {
+                configure.AddDebug();
+            });
+#endif
+
+            return builder.Build();
+        }
+    }
+}
